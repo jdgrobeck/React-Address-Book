f8f413d4a8abd17b50b0e67f253790dcee42f7e7
jdgrobeck <jdgrobeck@gmail.com>
2023-04-18 16:38:51 -0500

    added toggled comments

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -11,6 +11,12 @@ const Contacts = () => {
       .then((data) => {
         setContacts(data.results)
 
+
+        //DO NOT DO THIS IN THE FUTURE lol. WAY TOO COMPLICATED. Use isToggled with useState like 
+
+        // {isToggled && <p>information you want to show</p>}
+        //   <button onClick={() => setIsToggled(!isToggled)}>Show More</button>
+
         // Array(data.results.length) creates a new array equal to the number of contacts in the fetched data, which is the length of the results array
         // New array is filled with false values
         // New array's initial state is all false values
